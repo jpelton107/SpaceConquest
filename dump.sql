f1c69c670a59b4c8fcae5d3439fbe43fe0be5a8a
Joel <joelpelton@hotmail.com>
2013-12-05 11:48:12 -0600

    Add db dump

---
+++ dump.sql
@@ -0,0 +1,426 @@
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: spaceconquest
+-- ------------------------------------------------------
+-- Server version	5.5.32-0ubuntu0.12.10.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `buildings`
+--
+
+DROP TABLE IF EXISTS `buildings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `buildings` (
+  `id` int(22) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `building_id` tinyint(3) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `building` tinyint(3) DEFAULT NULL,
+  `quantity_building` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `building_id` (`building_id`),
+  CONSTRAINT `buildings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `buildings_ibfk_2` FOREIGN KEY (`building_id`) REFERENCES `buildings_reference` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buildings`
+--
+
+LOCK TABLES `buildings` WRITE;
+/*!40000 ALTER TABLE `buildings` DISABLE KEYS */;
+INSERT INTO `buildings` VALUES (1,8,1,10,0,0),(2,8,2,0,NULL,NULL),(3,8,3,0,NULL,NULL),(4,8,4,0,NULL,NULL),(5,8,5,0,NULL,NULL),(6,8,6,0,NULL,NULL),(7,8,7,0,NULL,NULL);
+/*!40000 ALTER TABLE `buildings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `buildings_reference`
+--
+
+DROP TABLE IF EXISTS `buildings_reference`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `buildings_reference` (
+  `id` tinyint(3) NOT NULL AUTO_INCREMENT,
+  `name` varchar(22) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `build_time` tinyint(3) NOT NULL DEFAULT '10',
+  `cost` tinyint(5) NOT NULL DEFAULT '100',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buildings_reference`
+--
+
+LOCK TABLES `buildings_reference` WRITE;
+/*!40000 ALTER TABLE `buildings_reference` DISABLE KEYS */;
+INSERT INTO `buildings_reference` VALUES (1,'Residence Quarter','Increases the amount of recruits available.',10,100),(2,'Crystal Mine','Increases the crystal production allowing more recruits to mine crystals',10,100),(3,'Dilithium Mine','Increases the dilithium production allowing more recruits to mine dilithium',10,100),(4,'Trading Post','Increases credit production by increasing the efficency from trade',10,100),(5,'Courthouse','Increases credit production by increasing the tax gathering efficency',10,100),(6,'Surface to Air Laser','The construction of SAL increases your overall defense.',10,100),(7,'Research Lab','Improves efficency of scientists allowing for quicker research and development.',10,100);
+/*!40000 ALTER TABLE `buildings_reference` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ci_sessions`
+--
+
+DROP TABLE IF EXISTS `ci_sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ci_sessions` (
+  `session_id` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  `ip_address` varchar(16) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  `user_agent` varchar(150) COLLATE utf8_bin NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ci_sessions`
+--
+
+LOCK TABLES `ci_sessions` WRITE;
+/*!40000 ALTER TABLE `ci_sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ci_sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `galaxy`
+--
+
+DROP TABLE IF EXISTS `galaxy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `galaxy` (
+  `id` int(22) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `galaxy`
+--
+
+LOCK TABLES `galaxy` WRITE;
+/*!40000 ALTER TABLE `galaxy` DISABLE KEYS */;
+INSERT INTO `galaxy` VALUES (1),(2);
+/*!40000 ALTER TABLE `galaxy` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login_attempts`
+--
+
+DROP TABLE IF EXISTS `login_attempts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `login_attempts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(40) COLLATE utf8_bin NOT NULL,
+  `login` varchar(50) COLLATE utf8_bin NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login_attempts`
+--
+
+LOCK TABLES `login_attempts` WRITE;
+/*!40000 ALTER TABLE `login_attempts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `login_attempts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `race`
+--
+
+DROP TABLE IF EXISTS `race`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `race` (
+  `id` tinyint(2) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `race`
+--
+
+LOCK TABLES `race` WRITE;
+/*!40000 ALTER TABLE `race` DISABLE KEYS */;
+INSERT INTO `race` VALUES (1,'Terran','More commonly known as humans. These barbaric creatures are out for blood, and receive a tremendous 15% attack bonus. They are over aggressive, and are not good at managing their economy. They lose 15% time while mining crystals, and dilithium.'),(2,'Thirgian','A primative race, but what they lack in technology they make up for in mining skill. This race receives a 15% boost to their mining, but they lose 15% resarch.'),(3,'Abjuko','The Abjuko are an incredibly intelligent, peaceful civilization. Nearly the entire population aspires to become a great scientist. They research technology 25% faster than Terrans, but they lose 25% when they decide to attack.');
+/*!40000 ALTER TABLE `race` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `resources`
+--
+
+DROP TABLE IF EXISTS `resources`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resources` (
+  `id` int(22) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `res_id` tinyint(3) NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT '5',
+  `miners` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `res_id` (`res_id`),
+  CONSTRAINT `resources_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `resources_ibfk_2` FOREIGN KEY (`res_id`) REFERENCES `resources_reference` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resources`
+--
+
+LOCK TABLES `resources` WRITE;
+/*!40000 ALTER TABLE `resources` DISABLE KEYS */;
+INSERT INTO `resources` VALUES (14,8,1,50,5),(15,8,2,25,2),(16,8,3,500,3);
+/*!40000 ALTER TABLE `resources` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `resources_reference`
+--
+
+DROP TABLE IF EXISTS `resources_reference`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resources_reference` (
+  `id` tinyint(3) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resources_reference`
+--
+
+LOCK TABLES `resources_reference` WRITE;
+/*!40000 ALTER TABLE `resources_reference` DISABLE KEYS */;
+INSERT INTO `resources_reference` VALUES (1,'Crystals'),(2,'Dilithium'),(3,'Credits');
+/*!40000 ALTER TABLE `resources_reference` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sector`
+--
+
+DROP TABLE IF EXISTS `sector`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sector` (
+  `id` int(22) unsigned NOT NULL AUTO_INCREMENT,
+  `sector` int(22) unsigned NOT NULL,
+  `galaxy_id` int(22) unsigned NOT NULL,
+  `leader_id` int(22) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `galaxy_id` (`galaxy_id`),
+  CONSTRAINT `sector_ibfk_1` FOREIGN KEY (`galaxy_id`) REFERENCES `galaxy` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sector`
+--
+
+LOCK TABLES `sector` WRITE;
+/*!40000 ALTER TABLE `sector` DISABLE KEYS */;
+INSERT INTO `sector` VALUES (1,1,1,0),(2,2,1,0),(3,3,1,0),(4,4,1,0),(5,1,2,0),(6,2,2,0),(7,3,2,0),(8,4,2,0);
+/*!40000 ALTER TABLE `sector` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ship`
+--
+
+DROP TABLE IF EXISTS `ship`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ship` (
+  `id` int(22) unsigned NOT NULL AUTO_INCREMENT,
+  `ref_id` int(22) NOT NULL,
+  `hull` int(11) NOT NULL,
+  `shield` int(11) NOT NULL,
+  `power` int(11) NOT NULL,
+  `travel` int(11) DEFAULT NULL,
+  `building` tinyint(3) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ship`
+--
+
+LOCK TABLES `ship` WRITE;
+/*!40000 ALTER TABLE `ship` DISABLE KEYS */;
+INSERT INTO `ship` VALUES (2,1,200,200,25,0,0,8);
+/*!40000 ALTER TABLE `ship` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ship_reference`
+--
+
+DROP TABLE IF EXISTS `ship_reference`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ship_reference` (
+  `id` int(22) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `image` varchar(64) NOT NULL,
+  `attack` int(22) NOT NULL,
+  `defense` int(22) NOT NULL,
+  `score` int(22) NOT NULL,
+  `shield` int(11) NOT NULL,
+  `hull` int(11) NOT NULL,
+  `power` int(11) NOT NULL,
+  `travel_time` tinyint(3) NOT NULL,
+  `build_time` tinyint(3) NOT NULL,
+  `cost_crystal` int(11) NOT NULL,
+  `cost_credits` int(11) NOT NULL,
+  `cost_dilithium` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ship_reference`
+--
+
+LOCK TABLES `ship_reference` WRITE;
+/*!40000 ALTER TABLE `ship_reference` DISABLE KEYS */;
+INSERT INTO `ship_reference` VALUES (1,'Zalo','img/zalo.png',5,1,10,200,200,25,4,4,10,100,3),(2,'Intercepter','img/intercepter.png',7,5,25,300,300,50,6,8,13,200,13),(3,'Fighter','img/fighter.png',12,1,25,200,200,40,4,10,25,200,2),(4,'Wraith','img/wraith.png',15,10,50,500,500,100,8,14,30,300,13),(5,'Corsair','img/corsair.png',20,18,100,1000,1000,200,12,18,40,500,30),(6,'Battlecruiser','img/battlecruiser.png',35,35,200,2000,2000,400,16,24,60,750,60);
+/*!40000 ALTER TABLE `ship_reference` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_autologin`
+--
+
+DROP TABLE IF EXISTS `user_autologin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_autologin` (
+  `key_id` char(32) COLLATE utf8_bin NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `user_agent` varchar(150) COLLATE utf8_bin NOT NULL,
+  `last_ip` varchar(40) COLLATE utf8_bin NOT NULL,
+  `last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`key_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_autologin`
+--
+
+LOCK TABLES `user_autologin` WRITE;
+/*!40000 ALTER TABLE `user_autologin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_autologin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_profiles`
+--
+
+DROP TABLE IF EXISTS `user_profiles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_profiles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `country` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `website` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_profiles`
+--
+
+LOCK TABLES `user_profiles` WRITE;
+/*!40000 ALTER TABLE `user_profiles` DISABLE KEYS */;
+INSERT INTO `user_profiles` VALUES (1,1,NULL,NULL),(2,2,NULL,NULL),(3,3,NULL,NULL),(4,4,NULL,NULL),(5,5,NULL,NULL),(6,6,NULL,NULL),(7,7,NULL,NULL),(8,8,NULL,NULL);
+/*!40000 ALTER TABLE `user_profiles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8_bin NOT NULL,
+  `email` varchar(100) COLLATE utf8_bin NOT NULL,
+  `sector_id` int(22) NOT NULL DEFAULT '1',
+  `race_id` tinyint(2) NOT NULL DEFAULT '1',
+  `vote_id` int(11) DEFAULT NULL,
+  `activated` tinyint(1) NOT NULL DEFAULT '1',
+  `banned` tinyint(1) NOT NULL DEFAULT '0',
+  `ban_reason` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `new_password_key` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `new_password_requested` datetime DEFAULT NULL,
+  `new_email` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `new_email_key` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `last_ip` varchar(40) COLLATE utf8_bin NOT NULL,
+  `last_login` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (8,'Joel','$2a$08$vDJp4wTJ/i077SNBiaX0je.IlIMpEMRrumMSuCiqwzag6PlZWgH0m','joelpelton@hotmail.com',8,1,NULL,1,0,NULL,NULL,NULL,NULL,NULL,'164.119.14.166','2013-12-05 09:22:51','2013-11-22 13:11:02','2013-12-05 15:22:51');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-12-05 11:47:29
