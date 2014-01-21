eaa41b9b7c0fc23394de7b7a0754130bb813eb33
Joel Pelton <joelpelton@hotmail.com>
2014-01-21 15:28:25 -0600

    Change overview to list advisors instead of fleet status... fleet status is separate page

--- application/views/cp/overview.php
+++ application/views/cp/overview.php
@@ -1,32 +1,8 @@
 <div class="content">
-	<table class="ship-display">
-		<tr>
-			<th>Class</th>
-			<th>Hull</th>
-			<th>Shield</th>
-			<th>Power</th>
-			<th>Status</th>
-		</tr>
-<?php 
-if ($user_ships):
-	foreach($user_ships as $user_ship): 
-?>
-		<tr>
-			<td><?php echo $user_ship['name'] ?></td>
-			<td><?php echo $user_ship['current_hull'] . "/" . $user_ship['max_hull'] ?></td>
-			<td><?php echo $user_ship['current_shield'] . "/" . $user_ship['max_shield'] ?></td>
-			<td><?php echo $user_ship['current_power'] . "/" . $user_ship['max_power'] ?></td>
-			<td>
-		<?php 
-		if ($user_ship['current_travel'] > 0) echo "Traveling (".$user_ship['current_travel']." ticks)";
-		elseif ($user_ship['current_building'] > 0) echo "Building (".$user_ship['current_building']." ticks)";
-		else echo "Defending";
-?>
-			</td>
-		</tr>
-<?php endforeach; 
-endif; ?>
-	</table>
+	<h4>Advisors</h4>
+	<p>Economy</p>
+	<p>Military</p>
+	<p>Society</p>
 </div>
 <div class="content notification">
 	<h4>Notifications</h4>
